11b8579b260d06d23ee33f367a2b2f51a86a5f51
qwertyyb <qwertyyb@foxmail.com>
2025-09-03 17:32:23 +0800

    feat: 添加src-node lint

--- src-node/src/index.ts
+++ src-node/src/index.ts
@@ -1,10 +1,10 @@
-import Koa from 'koa'
-import koaBody from '@koa/bodyparser'
-import cors from '@koa/cors'
-import { createServer } from "http";
+import Koa from 'koa';
+import koaBody from '@koa/bodyparser';
+import cors from '@koa/cors';
+import { createServer } from 'http';
 
-import managerRoutes from './routes/manager'
-import utilsRouter from './routes/utils'
+import managerRoutes from './routes/manager';
+import utilsRouter from './routes/utils';
 
 import createLoggerMiddleware from './middlewares/log';
 import createErrorHandler from './middlewares/err';
@@ -12,22 +12,22 @@ import { startSocketIO } from './socket.io';
 import createCacheMiddleware from './middlewares/cache';
 import logger from './utils/logger';
 
-const app = new Koa()
+const app = new Koa();
 
 app.use(koaBody({
-  jsonLimit: '100mb'
-}))
-app.use(cors())
+  jsonLimit: '100mb',
+}));
+app.use(cors());
 
-app.use(createLoggerMiddleware())
-app.use(createErrorHandler())
-app.use(createCacheMiddleware())
+app.use(createLoggerMiddleware());
+app.use(createErrorHandler());
+app.use(createCacheMiddleware());
 
-app.use(managerRoutes.routes()).use(managerRoutes.allowedMethods())
-app.use(utilsRouter.routes()).use(utilsRouter.allowedMethods())
+app.use(managerRoutes.routes()).use(managerRoutes.allowedMethods());
+app.use(utilsRouter.routes()).use(utilsRouter.allowedMethods());
 
 const httpServer = createServer(app.callback());
 
-startSocketIO(httpServer)
+startSocketIO(httpServer);
 
 httpServer.listen(2345, () => logger.info(`listen on ${2345}`));
