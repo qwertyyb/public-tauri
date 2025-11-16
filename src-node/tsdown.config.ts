b7b98741fc12541c59087c1f50c33306f9afff9e
qwertyyb <qwertyyb@foxmail.com>
2025-11-16 12:03:14 +0800

    feat: 升级依赖

--- src-node/tsdown.config.ts
+++ src-node/tsdown.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsdown';
+import pkg from './package.json' with { type: 'json' }
+
+const noBundleModules = ['@nut-tree-fork/nut-js', 'better-sqlite3']
 
 export default defineConfig({
   entry: ['./src/index.ts'],
   platform: 'node',
   shims: true,
+  noExternal: Object.keys(pkg.dependencies).filter(name => !noBundleModules.includes(name))
 });
