498fd752e6bddd9ce23dcb8b68e2dac1f6e2bdfc
qwertyyb <qwertyyb@qq.com>
2026-04-05 17:04:57 +0800

    feat: 引入插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_double_tap_shortcut::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_nspanel::init())
