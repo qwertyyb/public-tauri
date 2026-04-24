80fd22749fcc50456f77da83f7eab801cf703e75
qwertyyb <qwertyyb@foxmail.com>
2026-04-24 20:46:03 +0800

    feat: 修复问题

--- plugins/confetti/src/index.ts
+++ plugins/confetti/src/index.ts
@@ -1,9 +1,14 @@
 import { definePlugin, mainWindow, WebviewWindow } from '@public-tauri/api';
 
+let webview: InstanceType<typeof WebviewWindow> | null = null;
+
 export default definePlugin(() => ({
   async onAction() {
     await mainWindow.hide();
-    const webview = new WebviewWindow('my-label', {
+    if (webview) {
+      return;
+    }
+    webview = new WebviewWindow('confetti', {
       url: 'http://confetti.plugin.localhost:2345/assets/overlay/index.html',
       transparent: true,
       decorations: false,
@@ -11,18 +16,35 @@ export default definePlugin(() => ({
       skipTaskbar: true,
       focusable: false,
       focus: false,
+      shadow: false,
       alwaysOnTop: true,
+      center: true,
+      width: window.screen.width,
+      height: window.screen.height,
     });
-    webview.once('tauri://created', () => {
-      console.log('webview successfully created');
-    });
-    webview.once('tauri://error', (e) => {
+    const instance = webview;
+    instance.once('tauri://error', (e: unknown) => {
       console.log('an error happened creating the webview', e);
     });
-    await webview.setIgnoreCursorEvents(true);
-    await webview.setSimpleFullscreen(true);
+    await new Promise((resolve) => {
+      instance.once('tauri://webview-created', () => {
+        resolve(true);
+      });
+      setTimeout(() => {
+        resolve(false);
+      }, 200);
+    });
+    try {
+      await instance.setIgnoreCursorEvents(true);
+      await instance.setSimpleFullscreen(true);
+    } catch (err) {
+      console.log('error setting ignore cursor events and simple fullscreen', err);
+    }
+    console.log('webview set ignore cursor events and simple fullscreen');
     setTimeout(async () => {
-      await webview.destroy();
+      console.log('destroying webview');
+      await instance.destroy();
+      webview = null;
       console.log('webview destroyed');
     }, 6000);
   },
